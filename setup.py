820ba2490addac0e534c3fadbda9d00113df96a2
Dogan <dogan.ulus@imag.fr>
2017-06-19 18:59:44 +0200

    Initial python bindings

--- setup.py
+++ setup.py
@@ -43,7 +43,26 @@ setup (name = 'montre',
        author='Dogan Ulus',
        author_email='doganulus@gmail.com',
        packages=["montre"],
-       description = 'This is a Python binding',
+       license='GPLv3+',
+       classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+       description = 'This is a Python binding for timed pattern matching',
        url='http://pypi.python.org/pypi/montre/',
        py_modules = ["montre.algebra"],
        ext_modules = [module1],
